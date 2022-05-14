7e9424fa8a2df0c46734aecf8630019b34fdbb44
Anett David <davidanett@gmail.com>
2022-05-14 17:55:19 +0200

    Add registration and login form

---
+++ src/Controller/TasklistController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TasklistController extends AbstractController
+{
+    /**
+     * @Route("/", name="tasklists")
+     */
+    public function index(): Response
+    {
+        return $this->render('tasklist/index.html.twig', [
+            'controller_name' => 'TasklistController',
+        ]);
+    }
+}
